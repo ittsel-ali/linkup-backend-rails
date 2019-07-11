87e4cde274bc4c3b38ee397a444f1e5623e853af
ittsel-ali <ittsel1@gmail.com>
2019-07-11 13:14:06 -0400

    Setup relevant models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,22 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :user_locations
+  has_many :visitors, through: :user_locations
+  has_many :registered_places, through: :visitors
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-	class << self
-		def authenticate(email, password)
-	    user = User.find_for_database_authentication(:email => email)
-	    
-	    if user&.valid_for_authentication? { user.valid_password?(password) } && user&.active_for_authentication?
-	      user
-	    end
-	   end
- 	end
+
+
+  class << self
+    def authenticate(email, password)
+      user = User.find_for_database_authentication(:email => email)
+
+      if user&.valid_for_authentication? { user.valid_password?(password) } && user&.active_for_authentication?
+        user
+      end
+     end
+  end
 end
