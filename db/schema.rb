ac14f9f9363b5c01f24fb54efecb7cae57b6e4b2
ittsel-ali <ittsel1@gmail.com>
2019-07-13 00:27:29 -0400

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 2018_11_11_001210) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-
   create_table "images", force: :cascade do |t|
     t.string "file"
     t.string "image_type"
@@ -25,7 +24,6 @@ ActiveRecord::Schema.define(version: 2018_11_11_001210) do
     t.datetime "updated_at", null: false
     t.index ["imagable_type", "imagable_id"], name: "index_images_on_imagable_type_and_imagable_id"
   end
-
 
   create_table "oauth_access_tokens", force: :cascade do |t|
     t.integer "resource_owner_id"
@@ -43,11 +41,8 @@ ActiveRecord::Schema.define(version: 2018_11_11_001210) do
 
   create_table "posts", force: :cascade do |t|
     t.string "text"
-    t.string "price"
-    t.bigint "visitor_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["visitor_id"], name: "index_posts_on_visitor_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -61,7 +56,6 @@ ActiveRecord::Schema.define(version: 2018_11_11_001210) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 
   add_foreign_key "oauth_access_tokens", "users", column: "resource_owner_id"
 end
