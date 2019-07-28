13d19f2abd5ba5b8fef99b4db28b056d3910dd37
ittsel-ali <ittsel1@gmail.com>
2019-07-28 15:18:18 -0400

    Modified User registeration
    * Include first name and last name on user registration

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -33,7 +33,7 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:email, :password, :confirmation_password)
+    params.require(:user).permit(:email, :password, :confirmation_password, :first_name, :last_name)
   end
 
 end
